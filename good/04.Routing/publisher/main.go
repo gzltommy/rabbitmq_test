4637e20a77382be9e7a19d3509f744d7c7d73ecb
gouzuoluo <1046292959@qq.com>
2022-03-18 17:56:01 +0800

    3、路由模式  direct

--- good/04.Routing/publisher/main.go
+++ good/04.Routing/publisher/main.go
@@ -25,13 +25,13 @@ func main() {
 
 	//3、声明交换机
 	err = ch.ExchangeDeclare(
-		"测试广播交换机", // 交换机名字
-		"fanout",  // 交换机类型，fanout 发布订阅模式
-		true,      // 是否持久化
-		false,     // auto-deleted
-		false,     // internal
-		false,     // no-wait
-		nil,       // arguments
+		"测试路由模式交换机", // 交换机名字
+		"direct",    // 交换机类型，fanout 发布订阅模式
+		true,        // 是否持久化
+		false,       // auto-deleted
+		false,       // internal
+		false,       // no-wait
+		nil,         // arguments
 	)
 	failOnError(err, "Failed to declare an exchange")
 
@@ -39,10 +39,10 @@ func main() {
 	for i := 0; i < 6; i++ {
 		body := fmt.Sprintf("Hello! -- %d", i) // 消息内容
 		err = ch.Publish(
-			"测试广播交换机", // exchange（交换机名字，跟前面声明对应）
-			"",        // 路由参数，fanout 类型交换机，自动忽略路由参数，填了也没用。
-			false,     // mandatory
-			false,     // immediate
+			"测试路由模式交换机",                    // exchange（交换机名字，跟前面声明对应）
+			fmt.Sprintf("route_key_%d", i), // 路由参数，关键参数，决定你的消息会发送到那个队列。
+			false,                          // mandatory
+			false,                          // immediate
 			amqp.Publishing{
 				ContentType: "text/plain", // 消息内容类型，这里是普通文本
 				Body:        []byte(body), // 消息内容
